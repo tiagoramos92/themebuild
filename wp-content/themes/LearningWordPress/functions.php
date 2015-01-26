8b10a51e0f4bb360587b7b9ed44e73ad79a49f44
Tiago Ramos <tiagoramos92@gmail.com>
2015-01-26 12:12:27 +0000

    created child pages and a specific navigation menu to those child

--- wp-content/themes/LearningWordPress/functions.php
+++ wp-content/themes/LearningWordPress/functions.php
@@ -12,3 +12,27 @@ register_nav_menus(array(
 	'primary' => __('Primary Menu'),
 	'footer' => __('Footer Menu'),
 	));
+
+
+// Get top ancestor
+
+function get_top_ancestor_id() {
+
+	global $post;
+
+	if($post->post_parent){
+		$ancestors = array_reverse(get_post_ancestors($post->ID));
+		return $ancestors[0];
+
+	}
+	return $post->ID;
+}
+
+// Does page have children?
+function has_children() {
+	global $post;
+
+	$pages = get_pages('child_of=' . $post->ID);
+	return count($pages);
+
+}
