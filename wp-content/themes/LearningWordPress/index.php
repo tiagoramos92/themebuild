89debf98997651317c9a7e23eaf97218f49282b7
Tiago Ramos <tiagoramos92@gmail.com>
2015-01-26 12:59:40 +0000

    created post categories author and date

--- wp-content/themes/LearningWordPress/index.php
+++ wp-content/themes/LearningWordPress/index.php
@@ -7,6 +7,27 @@ if(have_posts()) :
 	
 	<article class="post">
 		<h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h2>
+		<p class="post-info"><?php the_time("F jS, Y | g:ia "); ?> by | 
+			<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
+			
+			<?php 
+
+			$categories = get_the_category();
+			$separator = ", ";
+			$output = '';
+
+			if($categories) {
+				foreach ($categories as $category) {
+					$output .='<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
+
+					//'<a href="' . get_category_link($category->term_id) . '"> '. $category->cat_name . '</a>'
+				}
+			}
+
+			echo trim($output, $separator);
+
+			?> 
+		</p>
 		<?php the_content(); ?>
 	</article>
 
